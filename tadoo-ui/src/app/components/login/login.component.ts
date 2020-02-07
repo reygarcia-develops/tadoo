ddd31fc417aa11b7752a05e559ddb45f39073787
Rey Garcia <reygarcia.develops@gmail.com>
2020-02-06 18:35:07 -0600

    Combined login/registration to switch between views easily. Added slide in animation. Now using FormBuilder

--- tadoo-ui/src/app/components/login/login.component.ts
+++ tadoo-ui/src/app/components/login/login.component.ts
@@ -1,25 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [
+    trigger('slideInAndFade', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(100%)' }),
+        animate('500ms ease-in', style({ transform: 'translateY(0%)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+      ]),
+    ]),
+  ]
 })
-export class LoginComponent implements OnInit {
-  username = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required])
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  registerForm = this.fb.group({
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    fullname: ['', [Validators.required]],
+  });
+
+  showLogin = true;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
 
   }
 
+  toggleViews() {
+    this.showLogin = !this.showLogin;
 
-  getErrorMessage() {
-    return this.username.hasError('required') ? 'Email is required' :
-      this.username.hasError('email') ? 'Invalid email' : '';
+    if (this.showLogin === true) {
+      this.registerForm.reset();
+    } else {
+      this.loginForm.reset();
+    }
   }
 }
