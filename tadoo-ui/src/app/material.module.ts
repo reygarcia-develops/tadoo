61996e0f79795bd63e7c814b642432e000b11408
Rey Garcia <reygarcia.develops@gmail.com>
2020-02-12 21:27:08 -0600

    Angular 9 update fixes

--- tadoo-ui/src/app/material.module.ts
+++ tadoo-ui/src/app/material.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
-import {
-  MatToolbarModule,
-  MatCardModule,
-  MatButtonModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatDividerModule,
-  MatSnackBarModule
-
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
