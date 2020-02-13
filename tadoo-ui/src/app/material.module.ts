cd5e54e7e2ee585f2e46c8b2b4b3960c26a7dba7
Rey Garcia <reygarcia.develops@gmail.com>
2020-02-12 21:44:40 -0600

    Fix missing material module

--- tadoo-ui/src/app/material.module.ts
+++ tadoo-ui/src/app/material.module.ts
@@ -6,7 +6,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   imports: [
